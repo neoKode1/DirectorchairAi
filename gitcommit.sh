bcff50355ce1891ffa3652f20ae558f8d8678a1d
neoKode1 <1deeptechnology@gmail.com>
2026-04-08 13:24:18 -0400

    chore: remove temp script

--- gitcommit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /Users/babypegasus/Desktop/prototypes/DirectorchairAi
-git add -A
-git status --short > /tmp/gitstatus.txt
-git commit -m "refactor: delete dead files and trim utils" > /tmp/gitcommit.txt 2>&1
-git push origin main > /tmp/gitpush.txt 2>&1
-echo "done" > /tmp/gitdone.txt
